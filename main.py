7e3a436aa024318a2842b13bd4e57022e5639901
DevHyung <khu[hj@gmail.com>
2017-12-28 03:29:19 +0900

    :memo: Search + parsing raw data sucess

--- main.py
+++ main.py
@@ -10,11 +10,31 @@ required module :selenium+chromewebdriver
 """
 from selenium import webdriver
 from HEADER import *
+import time
+from bs4 import BeautifulSoup
 if __name__=="__main__":
+    # Setting variable
     dir = './chromedriver'  # Driver Path
     driver = webdriver.Chrome(dir)
-    driver.get("https://cafe.naver.com/0404ab") # target page
+    # Login start
+    driver.get("http://cafe.naver.com/0404ab") # target page
     driver.find_elements_by_xpath('//*[@id="gnb_login_button"]/span[3]')[0].click()
     driver.find_elements_by_xpath('//*[@id="id"]')[0].send_keys(NAVER_ID)
     driver.find_elements_by_xpath('//*[@id="pw"]')[0].send_keys(NAVER_PW)
     driver.find_elements_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input')[0].click()
+    # Login end
+    # Search start
+    time.sleep(0.5)
+    driver.find_elements_by_xpath('//*[@id="cafe-menu"]/div[2]/ul[2]/li[1]')[0].click()
+    ## SwitchTo iframe
+    driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="cafe_main"]'))
+    time.sleep(0.5)
+    driver.find_element_by_id('query').send_keys(KEYWORD)
+    driver.find_elements_by_xpath('//*[@id="main-area"]/div[9]/form/a/img')[0].click()
+    # Search end
+    # Parsing start
+    time.sleep(0.5)
+    driver.find_elements_by_xpath('//*[@id="main-area"]/div[7]/form/table/tbody/tr[1]/td[2]/span/span/a')[0].click()
+    time.sleep(0.5)
+    bs4 = BeautifulSoup(driver.page_source,"lxml")
+    print(bs4.prettify())
