db18052cb74b81839ca674fc47bf5672e5d37fca
DevHyung <khu[hj@gmail.com>
2017-12-28 11:17:41 +0900

    :memo: add DB config

--- HEADER.py
+++ HEADER.py
@@ -1,2 +1,9 @@
 NAVER_ID = "INPUT_YOUR_NAVER_ID"
 NAVER_PW = "INPUT_YOUR_NAVER_PW"
+KEYWORD = "데코타일 시공 010"
+#___DB부분 변수
+ip = 'localhost'
+id = 'root'
+pw = 'autoset'
+name = 'outsourcing'
+chs ='utf8'
